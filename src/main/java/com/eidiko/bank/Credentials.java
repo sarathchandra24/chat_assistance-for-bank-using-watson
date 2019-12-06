dcb7a6f2654e4b9c6dd5ec51130a0bd13b2f2ff6
Sarath Chandra Oruganti <39090092+SarathChandra24@users.noreply.github.com>
2019-12-06 15:40:13 +0530

    Update Credentials.java

--- src/main/java/com/eidiko/bank/Credentials.java
+++ src/main/java/com/eidiko/bank/Credentials.java
@@ -2,9 +2,9 @@ package com.eidiko.bank;
 
 public class Credentials {
     public static String URL = "";
-    public static String user="sarath";
-    public static String password="T0nwhy@$$@";
-    public static String database = "eidiko_bank";
+    public static String user="";
+    public static String password="";
+    public static String database = "";
     public static String CreditCardTable="credit_card_table";
     public static String CustomerDetails="customer_details";
 }
